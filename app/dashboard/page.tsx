26fc3d212375c0e9fe7fb9194e4a1c380b49e718
apoorv pindwar <apoorvpal6302@gmail.com>
2025-04-19 03:00:20 +0530

    updated

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,4 +1,4 @@
-import { currentUser } from "@clerk/nextjs";
+import { currentUser } from "@clerk/nextjs/server";
 import { format } from "date-fns";
 import { 
   CircleUser, 
--- app/layout.tsx
+++ app/layout.tsx
@@ -40,3 +40,4 @@ export default function RootLayout({
     </ClerkProvider>
   );
 }
+// my name is apoorv
